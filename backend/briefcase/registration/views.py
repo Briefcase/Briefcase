2804ee0d703f0f31e492bc8b4ba60f023e028a0e
Beth Towns <townse2@rpi.edu>
2011-09-28 18:23:04 -0400

    missed a file a hadn't saved : /

--- backend/briefcase/registration/views.py
+++ backend/briefcase/registration/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 
 def index(request):
+    user = User.objects.create
     return HttpResponse("Hi there. Would you like a cookie? - New User Registration will hopefully go here")

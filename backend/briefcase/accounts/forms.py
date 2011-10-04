12a0f6bd382d847c4da4b0c54297b1b783a7f884
Elizabeth Towns <townse2@rpi.edu>
2011-10-04 18:24:16 -0400

    user registration seems to be working with the validators except it isn't displaying the errors

--- backend/briefcase/accounts/forms.py
+++ backend/briefcase/accounts/forms.py
@@ -1,21 +1,28 @@
 from django import forms
-from django.core import validators
+from django.core.exceptions import ValidationError
 from django.contrib.auth.models import User
 
-
+#validators
+def isValidUsername(value):
+    try:
+        User.objects.get(username = value)
+    except User.DoesNotExist:
+        return
+    raise validators.ValidationError('The username "$s" is already taken.' % value)
+        
 class RegistrationForm(forms.Form):
-        username = forms.CharField(max_length = 30)
+        username = forms.CharField(max_length = 30, validators = [isValidUsername])
         email = forms.EmailField(max_length =30)
-        password = forms.CharField(max_length = 30)
-        password_again = forms.CharField(max_length = 30)
+        password = forms.CharField(max_length = 30, widget=forms.PasswordInput)
+        password_again = forms.CharField(max_length = 30, widget = forms.PasswordInput)
+        
+        def clean(self):
+            cleaned_data = self.cleaned_data
+            password = cleaned_data.get("password")
+            password_again = cleaned_data.get("password_again")
+            if password and password_again:
+                if password!=password_again:
+                    raise forms.ValidationError("passwords must match")
+            return cleaned_data
  
-#need to look up how to do stuff with validators...kept getting errors 
-    # def passwordMatches(self, value):
-        # if value !=self.pw1:
-            # raise validators.ValidationError('Passwords must match.')
-    # def isValidUsername(self, value):
-        # try:
-            # User.objects.get(username = value)
-        # except User.DoesNotExist:
-            # return
-        # raise validators.ValidationError('The username "$s" is already taken.' % value)
+

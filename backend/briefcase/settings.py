a455c839034cbcdd758706c37c94cd29daa829fc
Beth Towns <townse2@rpi.edu>
2011-10-03 20:41:18 -0400

    started working on a logo- don't worry it won't be that red color, that was just for vectorizing

--- backend/briefcase/settings.py
+++ backend/briefcase/settings.py
@@ -103,7 +103,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'briefcase.urls'
 
 TEMPLATE_DIRS = (
-    "C:/Users/Bethee/Documents/RPI/fall2011/Briefcase/backend/briefcase/templates"
+    "/home/shared/briefcase/backend/briefcase/templates"
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.

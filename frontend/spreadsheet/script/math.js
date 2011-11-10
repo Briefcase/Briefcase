6924ce624e184428478289618ec3afef1ec1650c
Asher Glick <asher.glick@gmail.com>
2011-11-10 13:38:05 -0500

    created "maximum" function in math library

--- frontend/spreadsheet/script/math.js
+++ frontend/spreadsheet/script/math.js
@@ -5,3 +5,22 @@ function math_pow (base,exponent) {
   }
   return output;
 }
+
+function default_sum() {
+  var output = 0;
+  for (var i =0; i < arguments.length; i++){
+    output += arguments[i];
+  }
+  return output;
+}
+
+function math_max() {
+  var maximum = arguments[0];
+  for (var i = 1; i < arguments.length; i++) {
+    if (maximum < arguments[i]){
+      maximum = arguments[i];
+    }
+  }
+  return maximum;
+}
+

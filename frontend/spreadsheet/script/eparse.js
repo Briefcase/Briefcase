292a729afe201dffb0924a02f20799c7dbe1cb94
Asher Glick <asher.glick@gmail.com>
2011-11-11 11:01:41 -0500

    Created ability to access values of cells, no recursion or error checking

--- frontend/spreadsheet/script/eparse.js
+++ frontend/spreadsheet/script/eparse.js
@@ -129,6 +129,64 @@ function isDigit(character) {
           character == '0');
 }
 
+function isCapLetter (character) {
+  return (character == 'A' ||
+          character == 'B' ||
+          character == 'C' ||
+          character == 'D' ||
+          character == 'E' ||
+          character == 'F' ||
+          character == 'G' ||
+          character == 'H' ||
+          character == 'I' ||
+          character == 'J' ||
+          character == 'K' ||
+          character == 'L' ||
+          character == 'M' ||
+          character == 'N' ||
+          character == 'O' ||
+          character == 'P' ||
+          character == 'Q' ||
+          character == 'R' ||
+          character == 'S' ||
+          character == 'T' ||
+          character == 'U' ||
+          character == 'V' ||
+          character == 'W' ||
+          character == 'X' ||
+          character == 'Y' ||
+          character == 'Z');
+}
+
+function letterValue (character) {
+  if (character == 'A' || character == 'a') { return 0; }
+  else if (character == 'B' || character == 'b') { return 1; }
+  else if (character == 'C' || character == 'c') { return 2; }
+  else if (character == 'D' || character == 'd') { return 3; }
+  else if (character == 'E' || character == 'e') { return 4; }
+  else if (character == 'F' || character == 'f') { return 5; }
+  else if (character == 'G' || character == 'g') { return 6; }
+  else if (character == 'H' || character == 'h') { return 7; }
+  else if (character == 'I' || character == 'i') { return 8; }
+  else if (character == 'J' || character == 'j') { return 9; }
+  else if (character == 'K' || character == 'k') { return 10; }
+  else if (character == 'L' || character == 'l') { return 11; }
+  else if (character == 'M' || character == 'm') { return 12; }
+  else if (character == 'N' || character == 'n') { return 13; }
+  else if (character == 'O' || character == 'o') { return 14; }
+  else if (character == 'P' || character == 'p') { return 15; }
+  else if (character == 'Q' || character == 'q') { return 16; }
+  else if (character == 'R' || character == 'r') { return 17; }
+  else if (character == 'S' || character == 's') { return 18; }
+  else if (character == 'T' || character == 't') { return 19; }
+  else if (character == 'U' || character == 'u') { return 20; }
+  else if (character == 'V' || character == 'v') { return 21; }
+  else if (character == 'W' || character == 'w') { return 22; }
+  else if (character == 'X' || character == 'x') { return 23; }
+  else if (character == 'Y' || character == 'y') { return 24; }
+  else if (character == 'Z' || character == 'z') { return 25; }
+}
+
 function isFunction(block) {
   if (block[block.length-1] == ')' && !isDigit(block[0])) {
     // check for function
@@ -171,7 +229,6 @@ function isFunction(block) {
 ////////////////////////////////////////////////////////////////////////////////
 function _splitcomma (block) {
   var data = block.split(',');
-  alert ("INSIDE " + data);
   var parenCount = 0;
   for (var i = 0; i < data.length; i++) {
     for (var j = 0; j < data[i].length; j++) {
@@ -183,9 +240,7 @@ function _splitcomma (block) {
       }
     }
     if (parenCount > 0) {
-      alert("Merging next " + data[i]);
       data[i] += ',' + data.splice(i+1,1);
-      alert("Merged into " + data[i]);
       parenCount = 0;
       i--;
     }
@@ -193,21 +248,53 @@ function _splitcomma (block) {
   return data;
 }
 ////////////////////////////////////////////////////////////////////////////////
+//
+////////////////////////////////////////////////////////////////////////////////
+function isCell (block) {
+  var atNumbers = false;
+  var splitPosition = -1;
+  if (!isCapLetter(block[0])) {
+    return "false";
+  }
+  for (var i = 0; i < block.length; i++) {
+    if (isCapLetter(block[i]) && !atNumbers) {
+      continue;
+    }
+    atNumbers=true;
+    splitPosition = i;
+    if (isDigit(block[i])) {
+      continue;
+    }
+    return "false";
+  }
+  // Convert Letters to numbers
+  var letters = block.substring(0,splitPosition);
+  var resultingNumber = 0;
+  for (var i = 0; i < letters.length; i++) {
+    resultingNumber += Math.pow(26,i) * letterValue (letters[i]);
+  }
+  return (resultingNumber+','+block.substring(splitPosition,block.length));
+}
+////////////////////////////////////////////////////////////////////////////////
+//
+////////////////////////////////////////////////////////////////////////////////
+function getCellValue (cell) {
+  return data[cell];
+}
+////////////////////////////////////////////////////////////////////////////////
 // the find function parses an element of the arithmatic and determines if it //
 // is a function, a number, or a cell name                                    //
 ////////////////////////////////////////////////////////////////////////////////
 function _FIND(input){
-  // a string
+  // Input is a String
   if (_INQUOTES(input)) {
     return input.substring(1,input.length-1);
   }
-  // a function
+  // Input is a Function
   var functData = isFunction(input);
   if (functData != "false") {
-    // call function
-    ///var parsed = functData.split(',');
+    // Call the Function
     var parsed = _splitcomma (functData);
-    alert(parsed);
     var functionName = parsed.pop();
     if (window[functionName] == undefined) {
       alert("Function Not Found");
@@ -219,10 +306,26 @@ function _FIND(input){
       return window[functionName].apply(this,parsed);
     }
   }
-  // A string
+  // Input is a Cell Location
+  var cellData = isCell(input)
+  if (cellData != "false"){
+    var cellValue = getCellValue(cellData);
+    if (cellValue == undefined || cellValue == "") {
+      alert("cell is undefined");
+      return 0;
+    }
+    if (cellValue[0] == '=') {
+      return eparse(cellValue.substring(1,cellValue.length));
+    }
+    return cellValue;
+  }
+  // Input is a Number
   return parseInt(input);
 }
-
+////////////////////////////////////////////////////////////////////////////////
+// Resmath checks to see if the block is encapsulated in Parentheses, if so   //
+// then the function returns true, if not the function returns false          //
+////////////////////////////////////////////////////////////////////////////////
 function _RESMATH(block) {
   var pcout = 1;
   if (block[0] == '(' && block[block.length-1] == ')') {
@@ -236,6 +339,10 @@ function _RESMATH(block) {
   }
   return false;
 }
+////////////////////////////////////////////////////////////////////////////////
+// This function is very similar to resmath but it functions for quotes       //
+// instead of Parentheses                                                     //
+////////////////////////////////////////////////////////////////////////////////
 function _INQUOTES(block) {
   if (block.length < 2) {
     return false;

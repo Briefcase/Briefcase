4539535269e88916d39278f39542e1415132498d
Asher Glick <asher.glick@gmail.com>
2011-11-25 00:46:23 -0500

    began creating python file to turn javascript functions into a java-doc like page

---
+++ clientSide/spreadsheet/functionParser/convert.py
@@ -0,0 +1,23 @@
+template = open("output.html","r").read()
+#print template
+templateElements = template.split("<!-- # DELIMITER # -->")
+#print templateElements
+functions = open("math.js").read().split("function ")
+functions.pop(0)
+for function in functions:
+  functionIndex = functions.index(function) 
+  function = function[0:function.find(')')].split('(')
+  removeUnderscore = function[0].split('_',1)
+  function[0] = removeUnderscore[0]+'.'+removeUnderscore[1]
+  functions[functionIndex] = function
+
+outputData = templateElements[0]
+for function in functions:
+  outputData += function[0] + templateElements[1]
+  outputData += function[1] + templateElements[2]
+  outputData += templateElements[3]
+outputData += templateElements[4]
+
+outputFile = open("docs.html","w")
+outputFile.write(outputData);
+

a00b51014146949dc43e1145118c1b2f7bc37ffb
Asher Glick <asher.glick@gmail.com>
2012-01-28 16:04:07 -0500

    added csrf stuffs

--- clientSide/spreadsheet/script/saveload.js
+++ clientSide/spreadsheet/script/saveload.js
@@ -30,27 +30,58 @@ function save() {
   //xmlhttp.send(output);
   
   */
-  /*var serverURL = "/spreadsheet/save";
+  
+  $(document).ajaxSend(function(event, xhr, settings) {
+    function getCookie(name) {
+        var cookieValue = null;
+        if (document.cookie && document.cookie != '') {
+            var cookies = document.cookie.split(';');
+            for (var i = 0; i < cookies.length; i++) {
+                var cookie = jQuery.trim(cookies[i]);
+                // Does this cookie string begin with the name we want?
+                if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+    function sameOrigin(url) {
+        // url could be relative or scheme relative or absolute
+        var host = document.location.host; // host + port
+        var protocol = document.location.protocol;
+        var sr_origin = '//' + host;
+        var origin = protocol + sr_origin;
+        // Allow absolute or scheme relative URLs to same origin
+        return (url == origin || url.slice(0, origin.length + 1) == origin + '/') ||
+            (url == sr_origin || url.slice(0, sr_origin.length + 1) == sr_origin + '/') ||
+            // or any other URL that isn't scheme relative or absolute i.e relative.
+            !(/^(\/\/|http:|https:).*/.test(url));
+    }
+    function safeMethod(method) {
+        return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+    }
+
+    if (!safeMethod(settings.type) && sameOrigin(settings.url)) {
+        xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+    }
+});
+  
+  
+  var serverURL = "/spreadsheet/save";
   $.ajax({
     type: "POST",
     url: serverURL,
-		  data: "howdy!",
-		  dataType: "html",
-		  success: function(data){
-        if (xmlhttp.redyState == 4 && xmlhttp.status == 200) {
-          alert (xmlhttp.responseText);
-        }
-		  },
-		  error: function(html){alert("error")}
-  });*/
-  $.post("/spreadsheet/save", {
-    name: "hello",
-    friut, "apple"
-    },
-    function(data) {
-      alert(data);
-    }
-  );
+		data: "howdy!",
+		dataType: "html",
+		success: function(data){
+      if (xmlhttp.redyState == 4 && xmlhttp.status == 200) {
+        alert (xmlhttp.responseText);
+      }
+		},
+		error: function(html){alert("error")}
+  });
   
   
   // save to a local variable, probably not needed in the end

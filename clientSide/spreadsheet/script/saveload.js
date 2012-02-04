fa00a5d010cdb7de962a62667f36756f7b40e5f0
Asher Glick <asher.glick@gmail.com>
2012-02-04 18:50:04 -0500

    made the load function recieve the data from ajax to parse

--- clientSide/spreadsheet/script/saveload.js
+++ clientSide/spreadsheet/script/saveload.js
@@ -46,7 +46,7 @@ function save() {
     if (!safeMethod(settings.type) && sameOrigin(settings.url)) {
         xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
     }
-});
+  });
   
   
   var serverURL = "/spreadsheet/save";
@@ -63,12 +63,62 @@ function save() {
   
   
   // save to a local variable, probably not needed in the end
-  savedFile = output;
+  // savedFile = output;
 }
 /************************************ LOAD ************************************\
 |
 \******************************************************************************/
 function load() {
+  // DJANGO CSRF (cross site request forgery)
+  $(document).ajaxSend(function(event, xhr, settings) {
+    function getCookie(name) {
+        var cookieValue = null;
+        if (document.cookie && document.cookie != '') {
+            var cookies = document.cookie.split(';');
+            for (var i = 0; i < cookies.length; i++) {
+                var cookie = jQuery.trim(cookies[i]);
+                // Does this cookie string begin with the name we want?
+                if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+    function sameOrigin(url) {
+        // url could be relative or scheme relative or absolute
+        var host = document.location.host; // host + port
+        var protocol = document.location.protocol;
+        var sr_origin = '//' + host;
+        var origin = protocol + sr_origin;
+        // Allow absolute or scheme relative URLs to same origin
+        return (url == origin || url.slice(0, origin.length + 1) == origin + '/') ||
+            (url == sr_origin || url.slice(0, sr_origin.length + 1) == sr_origin + '/') ||
+            // or any other URL that isn't scheme relative or absolute i.e relative.
+            !(/^(\/\/|http:|https:).*/.test(url));
+    }
+    function safeMethod(method) {
+        return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+    }
+
+    if (!safeMethod(settings.type) && sameOrigin(settings.url)) {
+        xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+    }
+  });
+  
+  
+  var serverURL = "/spreadsheet/load";
+  $.ajax({
+    type: "POST",
+    url: serverURL,
+		data: "Give me the file or Ill shoot you",
+		dataType: "html",
+		success: function(data){
+        savedFile = data;
+		},
+		error: function(html){alert("error: "+html)}
+  });
   var test = JSON.parse(savedFile);
   delete data;
   

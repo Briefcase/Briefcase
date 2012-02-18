9e21084514ab8188d22f75d248ab8d2a1dfdf828
eliztowns <townse2@rpi.edu>
2012-02-18 15:47:21 -0500

    yay for failing to edit stuff

--- serverSide/urls.py
+++ serverSide/urls.py
@@ -5,10 +5,8 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^accounts/$', 'accounts.views.index'),
+    url(r'^accounts/$', 'accounts.views.userprofile'),
     url(r'^accounts/register', 'accounts.views.register'),
-    url(r'^accounts/login', 'accounts.views.userlogin'),
-    url(r'^accounts/logout','accounts.views.userlogout'),
     url(r'^accounts/uploadfile','accounts.views.save_file'),
     url(r'^$', 'accounts.views.index'),
     url(r'^spreadsheet/$', 'accounts.views.spreadsheet'),

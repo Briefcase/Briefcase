72fcc4a1ef9b22fa0888d7bc18900f84eba6a564
eliztowns <townse2@rpi.edu>
2012-02-18 15:58:18 -0500

    i dont understand

--- serverSide/accounts/views.py
+++ serverSide/accounts/views.py
@@ -46,7 +46,7 @@ def userprofile(request):
         'user_spreadsheet_list': user_spreadsheet_list,
         'current_user': request.user,
     })
-    return render_to_response('user_profile.html', context_instance = RequestContext(request)
+    return HttpResponse(t.render(c))
 
 def register(request):
     form = RegistrationForm()

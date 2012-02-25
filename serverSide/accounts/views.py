693b22689cb9e4f55ff762d65ad505ab5b9c459c
eliztowns <townse2@rpi.edu>
2012-02-25 16:38:04 -0500

    working on allowed users

--- serverSide/accounts/views.py
+++ serverSide/accounts/views.py
@@ -107,7 +107,7 @@ def load(request):
         fname=request.POST['filename']#get the name of requested file
         uname=request.POST['username'] #get the user that owns the file
         cur_profile=UserProfile.objects.get(user=request.user)
-        #own_profile=UserProfile.objects.get(user=uname)
+        own_profile=UserProfile.objects.get(user=uname)
         s=Spreadsheet.objects.get(pk=fname)
         # if s.public==True or cur_profile in s.allowed_users.all():
             # return HttpResponse(s.data) #send to frontend the entire file

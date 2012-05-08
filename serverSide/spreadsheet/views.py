05a9269473cafcbd9a579c42d71695f733e9f243
eliztowns <townse2@rpi.edu>
2012-05-08 17:54:05 -0400

    debugging

--- serverSide/spreadsheet/views.py
+++ serverSide/spreadsheet/views.py
@@ -44,7 +44,7 @@ def autosave(request):
         #find
         #return 
         return HttpResponse(json.dumps(cur_data))
-    else return HttpResponseBadRequest
+    else return HttpResponseBadRequest()
                 
                 
         

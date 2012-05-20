13d08ee9f0c9102a7aa6c5dfd1f8f9f0349b282f
Asher Glick <asher.glick@gmail.com>
2012-05-19 23:53:19 -0400

    beginning to work on the hotkey framework

--- clientSide/components/hotkeys/hotkeys.js
+++ clientSide/components/hotkeys/hotkeys.js
@@ -50,7 +50,8 @@
 | be able to use it and not have to deal with overwriting each other's         |
 | interrupt functions                                                          |
 \******************************************************************************/
-var _hot_key_list_ = new Array();
+var _HOT_KEY_LIST_ = new Array();
+
 
 function addHotkey(hotkey,functioncall) {
   

e7a787e1b923fc4c64d405d82c3bca75abdd1680
Asher Glick <asher.glick@gmail.com>
2012-05-27 22:22:47 -0400

    added a few comments

--- clientSide/components/hotkeys/hotkeys.js
+++ clientSide/components/hotkeys/hotkeys.js
@@ -56,7 +56,7 @@ var _HOT_KEY_DOWN_LIST_ = new Array();
 var regKeysReverse={48: "0",49: "1",50: "2",51: "3",52: "4",53: "5",54: "6",55: "7",56: "8",57: "9",59: ";",61: "=",65: "A",66: "B",67: "C",68: "D",69: "E",70: "F",71: "G",72: "H",73: "I",74: "J",75: "K",76: "L",77: "M",78: "N",79: "O",80: "P",81: "Q",82: "R",83: "S",84: "T",85: "U",86: "V",87: "W",88: "X",89: "Y",90: "Z"};
 var regKeys={"0": 48,"1": 49,"2": 50,"3": 51,"4": 52,"5": 53,"6": 54,"7": 55,"8": 56,"9": 57,";": 59,"=": 61,"A": 65,"B": 66,"C": 67,"D": 68,"E": 69,"F": 70,"G": 71,"H": 72,"I": 73,"J": 74,"K": 75,"L": 76,"M": 77,"N": 78,"O": 79,"P": 80,"Q": 81,"R": 82,"S": 83,"T": 84,"U": 85,"V": 86,"W": 87,"X": 88,"Y": 89,"Z": 90};
 
-
+//Key class for creating key objects
 var KeyObject = function (value) {
   this.shiftKey = false;
   this.metaKey = false;
@@ -92,6 +92,7 @@ var KeyObject = function (value) {
 }
 // Shift + Ctrl + Alt + Mod + Del
 
+// add a 
 function addKeyDown(hotkey,functioncall) {
   
 }

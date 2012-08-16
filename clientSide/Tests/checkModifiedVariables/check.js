a61a1290bf8608aa00d2c368d1ba3dc41ff26742
Asher Glick <asher.glick@gmail.com>
2012-08-16 16:38:57 -0400

    after working some hours on figuring this out I have made "onload" settable multiple times and they will all run

---
+++ clientSide/Tests/checkModifiedVariables/check.js
@@ -0,0 +1,20 @@
+//console.log(window.onload);
+
+onload = function () {alert("Not Loaded")};
+onload = function () {alert("Loaded")};
+
+
+// You probably dont want to have two onloads being set
+window._onload = [];
+window._onload.push(window.onload);
+window.onload=function(){for(a in _onload) {_onload[a]()}}
+Object.defineProperty(window,"onload", {
+	get: function () {return this._onload},
+	set: function (n) {this._onload.push(n);}
+});
+
+// Lets add a few more onloads to test the process
+onload = function () {alert("Loaded2")};
+onload = function () {alert("Loaded3")};
+
+console.log("ONLOAD:" + window.onload);

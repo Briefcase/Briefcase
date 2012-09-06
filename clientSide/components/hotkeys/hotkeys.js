9653749a7e488c9021dd3a394389d55e1592a044
Asher Glick <asher.glick@gmail.com>
2012-09-05 23:26:52 -0400

    worked on documenting the hotkey functions

--- clientSide/components/hotkeys/hotkeys.js
+++ clientSide/components/hotkeys/hotkeys.js
@@ -50,9 +50,12 @@
 | be able to use it and not have to deal with overwriting each other's         |
 | interrupt functions                                                          |
 \******************************************************************************/
+
+// a list of keypress event objects //
 var _HOT_KEY_UP_LIST_ = new Array();
 var _HOT_KEY_DOWN_LIST_ = new Array();
 
+// 
 var regKeysReverse={48: "0",49: "1",50: "2",51: "3",52: "4",53: "5",54: "6",55: "7",56: "8",57: "9",59: ";",61: "=",65: "A",66: "B",67: "C",68: "D",69: "E",70: "F",71: "G",72: "H",73: "I",74: "J",75: "K",76: "L",77: "M",78: "N",79: "O",80: "P",81: "Q",82: "R",83: "S",84: "T",85: "U",86: "V",87: "W",88: "X",89: "Y",90: "Z"};
 var regKeys={"0": 48,"1": 49,"2": 50,"3": 51,"4": 52,"5": 53,"6": 54,"7": 55,"8": 56,"9": 57,";": 59,"=": 61,"A": 65,"B": 66,"C": 67,"D": 68,"E": 69,"F": 70,"G": 71,"H": 72,"I": 73,"J": 74,"K": 75,"L": 76,"M": 77,"N": 78,"O": 79,"P": 80,"Q": 81,"R": 82,"S": 83,"T": 84,"U": 85,"V": 86,"W": 87,"X": 88,"Y": 89,"Z": 90};
 var specialKeys={"Tab": 9,"Backspace": 8,"F1": 112,"F2": 113,"F3": 114,"F4": 115,"F5": 116,"F6": 117,"F7": 118,"F8": 119,"F9": 120,"F10": 121,"F11": 122,"F12": 123,"Enter": 13,"Esc": 27,"PrintScreen": 42,"ScrollLock": 145,"Pause": 19,"Insert": 45,"Delete": 46,"Home": 36,"End": 35,"PageUp": 33,"PageDown": 34,"CapsLock": 20,"Up": 38,"Down": 40,"Left": 37,"Right": 39,"`": 192,"-": 109,"[": 219,"]": 221,"\\": 220,"'": 222,",": 188,".": 190,"/": 191,"Space": 32};
@@ -81,10 +84,11 @@ var KeyObject = function (value) {
     return output;
   }
   
-  // parse the input value
+  // parse the input value as a string //
   if (typeof(value) == 'string') {
     alert("stringman!");
   }
+  // parset the input value as a keyboard event //
   else if (value.toString() == '[object KeyboardEvent]') {
     this.charCode = value.charCode;
     this.keyCode = value.keyCode;
@@ -98,7 +102,7 @@ var KeyObject = function (value) {
 }
 // Shift + Ctrl + Alt + Mod + Del
 
-// add a 
+// Register a new key event //
 function addKeyDown(hotkey,functioncall) {
   
 }

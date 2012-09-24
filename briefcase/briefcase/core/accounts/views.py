1b9aaba14c806c7deaacff6a7865146c6488b891
Andrew Karnani <keeperguy64@gmail.com>
2012-09-24 16:41:17 -0400

    Accounts page now shows owner's files.
    Accounts also does not require allowed_users or view_only_users.
    
    fixes #19

--- briefcase/briefcase/core/accounts/views.py
+++ briefcase/briefcase/core/accounts/views.py
@@ -10,7 +10,8 @@ from briefcase.core import module
 @login_required
 def home(request):
     types = module.getTypes()
-    user_document_list = request.user.allowed_docs.all()
+    user_document_list = sorted(list(request.user.allowed_docs.all()) + list(request.user.owner.all()), key=lambda doc: doc.modified_date, reverse=True)
+
     user_view_list = request.user.view_only_docs.all()
     return render(request, "accounts/user_profile.html", {'current_user': request.user, 'user_document_list': user_document_list, 'user_view_list': user_view_list, "types":types})
 

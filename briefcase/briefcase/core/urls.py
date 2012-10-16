211fd23a7893c944ce308ca7bb2100871f1616ea
eliztowns <townse2@rpi.edu>
2012-10-16 17:59:50 -0400

    rename and delete

--- briefcase/briefcase/core/urls.py
+++ briefcase/briefcase/core/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
     url(r'^', include('briefcase.core.accounts.urls')),
     url(r'^spreadsheet/', include('briefcase.core.spreadsheet.urls')),
-    url(r'^delete/', 'views.delete'),
-    url(r'^rename/','views.rename'),
+    url(r'^delete/', 'briefcase.core.views.delete'),
+    url(r'^rename/','briefcase.core.views.rename'),
 
     )

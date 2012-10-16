8ee0d40150b50f06adbc41a70c3dada92b550e5b
Asher Glick <asher.glick@gmail.com>
2012-10-16 19:51:14 -0400

    jQuery is wierd, I am just sending this data as a string and it will be parsed on the backend

--- briefcase/briefcase/core/spreadsheet/static/spreadsheet/script/saveload.js
+++ briefcase/briefcase/core/spreadsheet/static/spreadsheet/script/saveload.js
@@ -149,7 +149,7 @@ function autosave() {
 \******************************************************************************/
 function devsave() {
   var fileid = getFileId();
-  var output = {"id":fileid,"spreadsheetcells":spreadsheetCells};
+  var output = {"id":fileid,"spreadsheetcells":JSON.stringify(spreadsheetCells)};
   var output 
   console.log(output);  
   var serverURL = "/spreadsheet/devsave/";

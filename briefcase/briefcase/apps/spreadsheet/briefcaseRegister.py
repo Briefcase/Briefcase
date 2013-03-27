3675f4ef78e1b419432c2432d8a140978567c8e7
Andrew Karnani <keeperguy64@gmail.com>
2013-03-27 17:33:53 -0400

    Updated spreadsheet briefcaseRegister to use new format

--- briefcase/briefcase/apps/spreadsheet/briefcaseRegister.py
+++ briefcase/briefcase/apps/spreadsheet/briefcaseRegister.py
@@ -1,4 +1,4 @@
 
 from briefcase import core
 
-core.module.register("Spreadsheet", "briefcase.apps.spreadsheet.views.new")
+core.module.register("Spreadsheet", "briefcase.apps.spreadsheet.views.new", r'^spreadsheet/', 'briefcase.apps.spreadsheet.urls')

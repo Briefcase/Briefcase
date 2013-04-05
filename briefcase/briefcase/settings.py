8186416fb1a9a752d8854a9e8aef53346561a22d
Josh Komoroske <jdkomo.dev@gmail.com>
2013-04-04 20:51:59 -0400

    Loog for sqlite3 df file in same directory as settings.py

--- briefcase/briefcase/settings.py
+++ briefcase/briefcase/settings.py
@@ -26,7 +26,7 @@ except ImportError:
 if DATABASES['default'] == {}:
     DATABASES['default'] = {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'briefcase_db.sqlite3',                      # Or path to database file if using sqlite3.
+        'NAME': os.path.join(BASE, 'briefcase_db.sqlite3'),                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.

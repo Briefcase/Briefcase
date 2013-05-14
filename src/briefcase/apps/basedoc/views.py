bfd18fabb0a791024134088d38925414c92dec9f
Asher Glick <asher.glick@gmail.com>
2013-05-14 14:22:25 -0400

    added comments to the basedoc template

--- src/briefcase/apps/basedoc/views.py
+++ src/briefcase/apps/basedoc/views.py
@@ -11,11 +11,20 @@ from session import Session, ClientList
 #list of spreadsheetsessions
 spreadsheetsessions = {}
 
+##################################### HOME #####################################
+# The home function is called when the user requests the actual application    #
+# page. It should server up the application tothe user.]                       #
+################################################################################
 @login_required
 def home(request, id):
     print "Hit Home Request"
     return render(request, "basedoc/basedoc.html")
 
+##################################### LOAD #####################################
+# The load function accepts a document ID and attempts to load the data from   #
+# that document and then return it to the user for the client side to display  #
+# as a document.                                                               #
+################################################################################
 @login_required
 def load(request):
     #
@@ -28,7 +37,10 @@ def load(request):
     return HttpResponse(data)
 
 
-# the new function is called whenever the user requests to create a new document of the given type
+###################################### NEW #####################################
+# The new function is called when a new file is requested to be created. It    #
+# intilizes the document and returns the new document's url to the user.       #
+################################################################################
 @login_required
 def new(request):
     newDocument = Basedoc(owner=request.user, file_name="New Basedoc", module="briefcase.apps.basedoc.views.home") # initlize the document
@@ -37,7 +49,13 @@ def new(request):
     url = {"url":reverse('briefcase.apps.basedoc.views.home', args=[newDocument.pk])} # Send the user back the url of the newly created document
     return HttpResponse(json.dumps(url))
    
-#"development" save - saves the whole document   
+############################### DEVELOPMENT SAVE ###############################
+#--------------------------------- DEPRECATED ---------------------------------#
+# This function was used for debugging when the database was being             #
+# configured. Now that the auto save function works there is no need for this  #
+# function to exist.                                                           #
+# This function Saves the entire document sent to it.                          #
+################################################################################
 @login_required
 def devsave(request):
     id=request.POST['id'] #pk of spreadsheet
